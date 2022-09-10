27130e740f52851c1f6ed2b292acbb06175bd14f
eyuphan-oguz <eyuphanoguz@yahoo.com>
2022-09-10 18:58:39 +0300

    Middle Use Case

--- lib/httpHello.dart
+++ lib/httpHello.dart
@@ -10,40 +10,56 @@ class HttpHelloView extends StatefulWidget {
 }
 
 class _HttpHelloViewState extends State<HttpHelloView> {
-   late bool isHaveData;
-   late String data;
+
    @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    isHaveData=false;
-    data='';
   }
 
   Future getApiJohnName()async{
-     var data= (await http.get(Uri.parse('https://hwasampleapi.firebaseio.com/api/books/0/author.json'))).body;
-     data=json.decode(data);
-
-     setState(() {
-       isHaveData=!isHaveData;
-       this.data=data;
-     });
+     return  (await http.get(Uri.parse('https://hwasampleapi.firebaseio.com/api/books/0/author.json'))).body;
 
 
+     // data=json.decode(data);
+     // setState(() {
+     //   isHaveData=!isHaveData;
+     //   this.data=data;
+     // });
   }
 
   @override
   Widget build(BuildContext context) {
+    // var columnCenter = Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         isHaveData? CircularProgressIndicator():Text(this.data),
+    //         ElevatedButton(onPressed: ()async{
+    //           await getApiJohnName();
+    //         }, child: Text('Send Request'))
+    //       ],
+    //     ),
+    //   );
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            isHaveData? CircularProgressIndicator():Text(this.data),
-            ElevatedButton(onPressed: ()async{
-              await getApiJohnName();
-            }, child: Text('Send Request'))
-          ],
+        child: FutureBuilder(
+          future: getApiJohnName(),
+          builder: (context,snapshot){
+            if(snapshot.connectionState==ConnectionState.done){
+                if(snapshot.hasData){
+                  return Text(json.decode(snapshot.data));
+                }else{
+                  return Text('You have error. Look at API');
+                }
+            }
+            else if(snapshot.connectionState==ConnectionState.waiting){
+              return CircularProgressIndicator();
+            }
+            else{
+              return Text('You have error? Are you sure API?');
+            }
+          },
         ),
       ),
     );
